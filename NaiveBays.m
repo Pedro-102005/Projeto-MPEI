a1f3af5d4406ad7d6ad3d2ac807abfd2107d243a
Pedro-102005 <p.fonseca@ua.pt>
2024-12-09 18:11:11 +0000

    melhorado

--- NaiveBays.m
+++ NaiveBays.m
@@ -1,57 +1,75 @@
+
 senhas_nao_seguras = readtable('passes_nao_seguras.csv', 'FileType', 'text', 'Delimiter', ',', 'Encoding', 'latin1');
 senhas_nao_seguras = table2array(senhas_nao_seguras); 
+senhas_nao_seguras_treino = senhas_nao_seguras(1:3000);
+senhas_nao_seguras_teste = senhas_nao_seguras(3001:end); % Ajustado o índice
 
 senhas_seguras = readtable('passes_seguras.csv', 'FileType', 'text', 'Delimiter', ',', 'Encoding', 'latin1');
 senhas_seguras = table2array(senhas_seguras); 
+senhas_seguras_treino = senhas_seguras(1:3000);
+senhas_seguras_teste = senhas_seguras(3001:end); % Ajustado o índice
 
+dados_nao_seguras = zeros(length(senhas_nao_seguras_treino), 5);
+dados_seguras = zeros(length(senhas_seguras_treino), 5);
 
-dados_nao_seguras = zeros(length(senhas_nao_seguras), 5);
-dados_seguras = zeros(length(senhas_seguras), 5);
-
-
-for i = 1:length(senhas_nao_seguras)
-    senha = senhas_nao_seguras{i}; 
+for i = 1:length(senhas_nao_seguras_treino)
+    senha = senhas_nao_seguras_treino{i}; 
     dados_nao_seguras(i, :) = dados_passe(senha); 
 end
 
-for i = 1:length(senhas_seguras)
-    senha = senhas_seguras{i}; 
-    dados_seguras(i, :) = dados_passe(senha);
+for i = 1:length(senhas_seguras_treino)
+    senha = senhas_seguras_treino{i}; 
+    dados_seguras(i, :) = dados_passe(senha); 
 end
 
-dados = [dados_nao_seguras; dados_seguras];
+dados_treino = [dados_nao_seguras; dados_seguras];
+rotulos_treino = [zeros(length(senhas_nao_seguras_treino), 1); ones(length(senhas_seguras_treino), 1)];
 
-defenicao = [zeros(length(senhas_nao_seguras), 1); ones(length(senhas_seguras), 1)];
+% Probabilidades a priori
+prob_segura = sum(rotulos_treino == 1) / length(rotulos_treino);
+prob_nao_segura = 1 - prob_segura;
+
+% Probabilidades condicionais (com smoothing para evitar divisões por zero)
+num_dados = size(dados_treino, 2);
+prob_passe_dado_seguro = (sum(dados_treino(rotulos_treino == 1, :)) + 1) ./ (sum(rotulos_treino == 1) + num_dados);
+prob_passe_dado_nao_seguro = (sum(dados_treino(rotulos_treino == 0, :)) + 1) ./ (sum(rotulos_treino == 0) + num_dados);
 
 
-% Probabilidade
-prob_segura = sum(defenicao == 1) / length(defenicao);
-prob_nao_segura = 1-prob_segura;
+previsoes_certas = 0;
+previsao_naoseguras = 0;
 
-% Probabilidade condicional
-num_dados = size(dados, 2);
-prob_passe_dado_seguro = sum(dados(defenicao == 1, :)) ./ sum(defenicao == 1);
-prob_passe_dado_nao_seguro = sum(dados(defenicao == 0, :)) ./ sum(defenicao == 0);
-
-% Pede ao utilizador
-senha = input('Insira a senha para verificar: ', 's');
-
-% Validações
-if ~all(isstrprop(senha, 'print'))
-    error('A senha contém caracteres inválidos.');
+for i = 1:length(senhas_seguras_teste)
+    senha = senhas_seguras_teste{i};
+    
+    if ~all(isstrprop(senha, 'print'))
+        error('A senha contém caracteres inválidos.');
+    end
+    
+    dados_senha = dados_passe(senha);
+    
+   
+    if log_probabilidade_ser_segura > log_probabilidade_nao_segura
+        previsoes_certas = previsoes_certas + 1;
+    end
 end
 
-dados_senha = dados_passe(senha);
+% Precisão
+precisao = previsoes_certas / length(senhas_seguras_teste);
+fprintf('Precisão do classificador: %.2f%%\n', precisao * 100);
 
-probabilidade_ser_segura = prod(prob_passe_dado_seguro(dados_senha == 1)) * prod(1 - prob_passe_dado_seguro(dados_senha == 0));
-probabilidade_nao_segura = prod(prob_passe_dado_nao_seguro(dados_senha == 1)) * prod(1 - prob_passe_dado_nao_seguro(dados_senha == 0));
+for i = 1:length(senhas_nao_seguras_teste)
+    senha = senhas_nao_seguras_teste{i};
+    
+    
+    
+    dados_senha = dados_passe(senha);
+        
 
-% P(classe/evidencia) = P(Evidencia/classe) * P(Classe)
-posterior_segura = probabilidade_ser_segura * prob_segura;
-posterior_nao_segura = probabilidade_nao_segura * prob_nao_segura;
-
-if posterior_segura > posterior_nao_segura
-    disp('A senha é segura.');
-else
-    disp('A senha não é segura.');
+    if log_probabilidade_ser_segura > log_probabilidade_nao_segura
+        previsao_naoseguras = previsao_naoseguras + 1;
+    end
 end
+
+precisao_outra = previsao_naoseguras / length(senhas_nao_seguras_teste);
+fprintf('Precisão do classificador nao seguras: %.2f%%\n', precisao * 100);
+
